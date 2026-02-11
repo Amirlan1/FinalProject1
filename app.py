8e6ad50da5c3e71eaf7da989ce2c0ab491a7d979
Asnk757 <asanali20062007@gmail.com>
2026-02-12 00:50:06 +0500

    убрал
    Убрал dotenv в app.py

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ import threading
 import requests
 import pandas as pd
 
-load_dotenv()
 api_key = "d668impr01qots73m1e0d668impr01qots73m1eg" 
 url = f"https://finnhub.io/api/v1/news?category=general&token={api_key}"
 
